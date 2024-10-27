3843dad04baf5b37212d7e3bbef8d62bfe91c3cf
TheLsbt <tyresewolmarans@gmail.com>
2024-10-27 11:28:56 +0200

    Ignore

--- src/Classes/ResourceProject.gd
+++
@@ -1,8 +0,0 @@
-class_name ResourceProject
-extends Project
-
-signal resource_updated
-
-
-func _init(_frames: Array[Frame] = [], _name := tr("untitled"), _size := Vector2i(64, 64)) -> void:
-	super._init(_frames, _name + " (Virtual Resource)", _size)
